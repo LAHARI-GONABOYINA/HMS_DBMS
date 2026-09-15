42353b920782d077e58bf5e635060c79c06faebd
Lalitha Boyidi <lalithaboyidi@gmail.com>
2026-09-15 10:12:17 +0530

    added delete data operations

---
+++ 04_Delete_Data.sql
@@ -0,0 +1,17 @@
+-- =========================================
+-- INSERT TEMPORARY PATIENT
+-- =========================================
+
+INSERT INTO patients (patient_id, first_name, last_name, dob, gender, phone, email, address)
+VALUES (106, 'Test', 'Patient', DATE '2000-01-01', 'Other', '9000000006', 'testpatient@gmail.com', 'Hyderabad');
+
+
+-- =========================================
+-- DELETE TEMPORARY PATIENT
+-- =========================================
+
+DELETE FROM patients
+WHERE patient_id = 106;
+
+
+COMMIT;
